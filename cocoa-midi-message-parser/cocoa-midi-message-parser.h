70685986ab673199426e4a872d5d64316dff5767
Nathan Hill <nathan.hill@inin.com>
2016-12-24 01:29:07 -0500

    Initial Commit

---
+++ cocoa-midi-message-parser/cocoa-midi-message-parser.h
@@ -0,0 +1,19 @@
+//
+//  cocoa-midi-message-parser.h
+//  cocoa-midi-message-parser
+//
+//  Created by Hill, Nathan on 12/24/16.
+//  Copyright © 2016 Hill, Nathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cocoa-midi-message-parser.
+FOUNDATION_EXPORT double cocoa_midi_message_parserVersionNumber;
+
+//! Project version string for cocoa-midi-message-parser.
+FOUNDATION_EXPORT const unsigned char cocoa_midi_message_parserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cocoa_midi_message_parser/PublicHeader.h>
+
+
